e31244b726df34d1080be1a26c8465def19e5b80
hoyoung lee <hyleekr9580@gmail.com>
2016-01-25 12:11:51 +0900

    첫 번째 커밋

---
+++ src/ArraylistExam.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+/**
+ * Created by 호영 on 2016-01-25.
+ */
+public class ArraylistExam {
+    public static void main(String[] args) {
+
+
+        Picture pic1 = new Picture("degas1.jpg");
+        Picture pic2 = new Picture("gaugin1.jpg");
+        Picture pic3 = new Picture("monet1.jpg");
+        Picture pic4 = new Picture("monet2.jpg");
+        Picture pic5 = new Picture("renoir1.jpg");
+
+        //ArrayList<Picture> 생성하고 add 하고
+        ArrayList<Picture> list = new ArrayList<>();
+        list.add(pic1);
+        list.add(pic2);
+        list.add(pic3);
+        list.add(pic4);
+        list.add(pic5);
+
+        //getMaxX() : 오른쪽 끝의 x좌표
+        //getWidth() : 가로 길이
+        //translate(x, y) : x, y만큼 그림을 이동
+        System.out.println(pic1.getMaxX());
+        // 사진의 좌표를 알 수 있다.
+        pic1.translate(0,0);
+        System.out.println(pic1.getMaxX());
+        pic1.draw();
+
+        // 사진의 사이즈를 확인 할 수 있다.
+//        System.out.println(pic1.getWidth());
+
+        // for 문으로 draw()
+        // list 사이즈를 알고 싶으면 list.size();
+//        int num = list.size();
+//        for (int i = 0; i < num; i++) {
+//            Picture pic = list.get(i);
+//            pic.draw();
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add(2, "d");
+
+        String str = "e";
+        list.add(str);
+
+        System.out.println(list.remove(2) + "삭제");
+        if (list.remove(str)){
+            System.out.println("삭제되었음");
+        }
+
+        System.out.println(list.get(0));
+
+        System.out.println(list);
+
+    }
+
+}
